88658a06847a0987375b4e9de823da33344630ec
Tharushika Thilakarathna <sttharuu123@gmail.com>
2026-08-18 09:41:03 +0530

    feat: add rest framework code

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,6 +125,10 @@ DATABASES = {
 # ============================================================
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
