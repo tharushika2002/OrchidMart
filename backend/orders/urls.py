85f3c1a4e223d8ad41fb87f6b57c727f3c74f0a3
Tharushika Thilakarathna <sttharuu123@gmail.com>
2026-08-24 15:11:23 +0530

    feat: create new urls for orders

---
+++ backend/orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import CreateOrderView
+
+
+urlpatterns = [
+    path(
+        "create/",
+        CreateOrderView.as_view(),
+        name="create-order",
+    ),
+]
